9b8caee231374706e09a93c53b3bd45dd0692a4f
jhonnierandrey <jhonnierandrey@outlook.com>
2023-05-11 22:47:20 -0300

    06 - controllers

---
+++ src/controllers/workoutController.ts
@@ -0,0 +1,50 @@
+import WorkoutModel from '../models/workoutModel.js';
+import mongoose from 'mongoose';
+
+// GET all workouts
+const getWorkouts = async (req, res) => {
+    const workouts = await WorkoutModel.find({}).sort({ createdAt: -1 })
+
+    res.status(200).json(workouts);
+}
+
+// GET a single workout
+const getWorkoutById = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'Workout not available' });
+    }
+
+    const workout = await WorkoutModel.findById(id);
+
+    if (!workout) {
+        return res.status(404).json({ error: 'Workout not available' });
+    }
+
+    res.status(200).json(workout);
+}
+
+// create a workout
+
+const createWorkout = async (req, res) => {
+    const { title, load, reps } = req.body;
+    try {
+        const workout = await WorkoutModel.create({
+            title, load, reps
+        })
+        res.status(200).json(workout)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+}
+
+// delete a workout
+
+// update a workout
+
+export {
+    createWorkout,
+    getWorkouts,
+    getWorkoutById
+}
--- src/routes/workouts.ts
+++ src/routes/workouts.ts
@@ -1,30 +1,16 @@
 import express from 'express';
-import WorkoutModel from '../models/workoutModel.js';
+import { createWorkout, getWorkouts, getWorkoutById } from '../controllers/workoutController.js';
 
 const router = express.Router()
 
 // Retreive all workouts
-router.get('/', (req, res) => {
-    res.json({ msg: 'GET all workouts' })
-})
+router.get('/', getWorkouts);
 
 // Retreive single workout
-router.get('/:id', (req, res) => {
-    res.json({ msg: 'GET a single workout' })
-})
+router.get('/:id', getWorkoutById);
 
 // create a new workout
-router.post('/', async (req, res) => {
-    const { title, load, reps } = req.body;
-    try {
-        const workout = await WorkoutModel.create({
-            title, load, reps
-        })
-        res.status(200).json(workout)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+router.post('/', createWorkout);
 
 // delete a workout
 router.delete('/:id', (req, res) => {
