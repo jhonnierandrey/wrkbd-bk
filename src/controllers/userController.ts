fa6e3e8a2f5ffeec7862aba4b35e12f78f33039f
jhonnierandrey <jhonnierandrey@outlook.com>
2023-05-16 21:40:38 -0300

    auth-03 - signing up and hashing passsword

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import UserModel from '../models/userModel.js';
+import bcrypt from 'bcrypt'
 
 // login user
 const loginUser = async (req, res) => {
@@ -7,7 +8,23 @@ const loginUser = async (req, res) => {
 
 // signup user 
 const singUpUser = async (req, res) => {
-    res.json({ msg: 'signup user' })
+    const { email, password } = req.body;
+    try {
+
+        const exists = await UserModel.findOne({ email })
+
+        if (exists) {
+            return res.status(400).json({ error: 'Email already in use' });
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt);
+
+        const user = await UserModel.create({ email, password: hash })
+        res.status(200).json({ email, user })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 export {
